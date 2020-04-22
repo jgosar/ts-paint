f74ad6f85a23092a6c91199801933ad35124729d
jgosar <36840705+jgosar@users.noreply.github.com>
2020-04-22 18:06:34 +0200

    Solved problems with too large paste canvas, simplified ts-paint component DOM

--- ts-paint/src/app/components/zoomable-canvas/zoomable-canvas.component.ts
+++ ts-paint/src/app/components/zoomable-canvas/zoomable-canvas.component.ts
@@ -14,6 +14,8 @@ export class ZoomableCanvasComponent implements OnChanges {
   @Input()
   image: ImageData;
   @Input()
+  parentImage: ImageData;
+  @Input()
   offset: Point = { w: 0, h: 0 };
   @ViewChild('imageCanvas', { static: true })
   imageCanvas: ElementRef;
@@ -24,13 +26,30 @@ export class ZoomableCanvasComponent implements OnChanges {
   zoomedOffsetH: number = 0;
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.zoomedWidth = (this.image?.width ?? 0) * this.zoom;
-    this.zoomedHeight = (this.image?.height ?? 0) * this.zoom;
-    this.zoomedOffsetW = (this.offset?.w ?? 0) * this.zoom;
-    this.zoomedOffsetH = (this.offset?.h ?? 0) * this.zoom;
+    if (this.imageCanvas) {
+      const canvas = this.imageCanvas.nativeElement;
+      const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
-    if (changes.image && this.image && this.imageCanvas) {
-      loadImageToCanvas(this.image, this.imageCanvas.nativeElement);
+      if (this.image) {
+        const parentWidth: number = (this.parentImage || this.image).width;
+        const parentHeight: number = (this.parentImage || this.image).height;
+
+        const canvasWidth: number = this.offset.w < 0 ? this.image.width + this.offset.w : Math.min(this.image.width, parentWidth - this.offset.w);
+        const canvasHeight: number = this.offset.h < 0 ? this.image.height + this.offset.h : Math.min(this.image.height, parentHeight - this.offset.h);
+
+        this.zoomedWidth = canvasWidth * this.zoom;
+        this.zoomedHeight = canvasHeight * this.zoom;
+        this.zoomedOffsetW = Math.max((this.offset?.w ?? 0), 0) * this.zoom;
+        this.zoomedOffsetH = Math.max((this.offset?.h ?? 0), 0) * this.zoom;
+
+        //loadImageToCanvas(this.image, this.imageCanvas.nativeElement);
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        context.putImageData(this.image, Math.min(this.offset.w, 0), Math.min(this.offset.h, 0));
+      } else {
+        canvas.width = 0;
+        canvas.height = 0;
+      }
     }
   }
 }
