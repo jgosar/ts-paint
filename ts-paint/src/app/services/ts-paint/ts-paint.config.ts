76f5440ad1597b91b0cf184240232e0b7f2800b8
jgosar <36840705+jgosar@users.noreply.github.com>
2020-07-17 18:39:30 +0200

    Moved large config objects to config file

---
+++ ts-paint/src/app/services/ts-paint/ts-paint.config.ts
@@ -0,0 +1,266 @@
+import { Color } from 'src/app/types/base/color';
+import { DrawingToolType } from 'src/app/types/drawing-tools/drawing-tool-type';
+import { MenuActionType } from 'src/app/types/menu/menu-action-type';
+import { MenuItem } from 'src/app/types/menu/menu-item';
+
+export const DEFAULT_AVAILABLE_COLORS: Color[] = [
+  { r: 0, g: 0, b: 0 },
+  { r: 255, g: 255, b: 255 },
+  { r: 128, g: 128, b: 128 },
+  { r: 196, g: 196, b: 196 },
+  { r: 128, g: 0, b: 0 },
+  { r: 255, g: 0, b: 0 },
+  { r: 128, g: 128, b: 0 },
+  { r: 255, g: 255, b: 0 },
+  { r: 0, g: 128, b: 0 },
+  { r: 0, g: 255, b: 0 },
+  { r: 0, g: 128, b: 128 },
+  { r: 0, g: 255, b: 255 },
+  { r: 0, g: 0, b: 128 },
+  { r: 0, g: 0, b: 255 },
+  { r: 128, g: 0, b: 128 },
+  { r: 255, g: 0, b: 255 },
+];
+
+export const DRAWING_TOOLS: DrawingToolType[] = [
+  DrawingToolType.rectangleSelect,
+  DrawingToolType.colorFiller,
+  DrawingToolType.colorPicker,
+  DrawingToolType.magnifier,
+  DrawingToolType.pencil,
+  DrawingToolType.line,
+  DrawingToolType.rectangle,
+  DrawingToolType.ellipse,
+];
+
+export const MENU_STRUCTURE: MenuItem[] = [
+  {
+    name: 'File',
+    menus: [
+      {
+        name: 'New',
+        hotkeys: ['Ctrl', 'N'],
+        action: MenuActionType.NEW,
+      },
+      {
+        name: 'Open...',
+        hotkeys: ['Ctrl', 'O'],
+        action: MenuActionType.OPEN_FILE,
+      },
+      {
+        name: 'Save',
+        hotkeys: ['Ctrl', 'S'],
+        action: MenuActionType.SAVE_FILE,
+      },
+      {
+        name: 'Save As...',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'Print Preview',
+        disabled: true,
+      },
+      {
+        name: 'Page Setup...',
+        disabled: true,
+      },
+      {
+        name: 'Print...',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'Set As Wallpaper (Tiled)',
+        disabled: true,
+      },
+      {
+        name: 'Set As Wallpaper (Centered)',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'Recent file',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'Exit',
+        disabled: true,
+      },
+    ],
+  },
+  {
+    name: 'Edit',
+    menus: [
+      {
+        name: 'Undo',
+        hotkeys: ['Ctrl', 'Z'],
+        action: MenuActionType.UNDO,
+      },
+      {
+        name: 'Repeat',
+        hotkeys: ['Ctrl', 'Y'],
+        action: MenuActionType.REPEAT,
+      },
+      {},
+      {
+        name: 'Cut',
+        hotkeys: ['Ctrl', 'X'],
+        action: MenuActionType.CUT,
+      },
+      {
+        name: 'Copy',
+        hotkeys: ['Ctrl', 'C'],
+        action: MenuActionType.COPY,
+      },
+      {
+        name: 'Paste',
+        hotkeys: ['Ctrl', 'V'],
+        disabled: true,
+      },
+      {
+        name: 'Clear Selection',
+        hotkeys: ['Delete'],
+        action: MenuActionType.CLEAR_SELECTION,
+      },
+      {
+        name: 'Select All',
+        hotkeys: ['Ctrl', 'A'],
+        action: MenuActionType.SELECT_ALL,
+      },
+      {},
+      {
+        name: 'Copy To',
+        disabled: true,
+      },
+      {
+        name: 'Paste From...',
+        disabled: true,
+      },
+    ],
+  },
+  {
+    name: 'View',
+    menus: [
+      {
+        name: 'Tool Box',
+        disabled: true,
+      },
+      {
+        name: 'Color Box',
+        disabled: true,
+      },
+      {
+        name: 'Status Bar',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'Zoom',
+        disabled: true,
+        menus: [
+          {
+            name: 'Normal Size',
+            disabled: true,
+          },
+          {
+            name: 'Large Size',
+            disabled: true,
+          },
+          {
+            name: 'Custom...',
+            disabled: true,
+          },
+          {},
+          {
+            name: 'Show Grid',
+            disabled: true,
+          },
+          {
+            name: 'Show Thumbnail',
+            disabled: true,
+          },
+        ],
+      },
+      {
+        name: 'View Bitmap',
+        disabled: true,
+      },
+      {
+        name: 'Text Toolbar',
+        disabled: true,
+      },
+    ],
+  },
+  {
+    name: 'Image',
+    menus: [
+      {
+        name: 'Flip/Rotate',
+        hotkeys: ['Ctrl', 'R'],
+        action: MenuActionType.FLIP_IMAGE,
+      },
+      {
+        name: 'Stretch/Skew',
+        disabled: true,
+      },
+      {
+        name: 'Invert Colors',
+        hotkeys: ['Ctrl', 'I'],
+        action: MenuActionType.INVERT_COLORS,
+      },
+      {
+        name: 'Attributes',
+        action: MenuActionType.OPEN_ATTRIBUTES_WINDOW,
+      },
+      {
+        name: 'Clear Image',
+        action: MenuActionType.CLEAR_IMAGE,
+      },
+    ],
+  },
+  {
+    name: 'Options',
+    menus: [
+      {
+        name: 'Edit Colors',
+        disabled: true,
+      },
+      {
+        name: 'Get Colors',
+        disabled: true,
+      },
+      {
+        name: 'Save Colors',
+        disabled: true,
+      },
+      {
+        name: 'Draw Opaque',
+        disabled: true,
+      },
+    ],
+  },
+  {
+    name: 'Help',
+    menus: [
+      {
+        name: 'Help Topics',
+        disabled: true,
+      },
+      {},
+      {
+        name: 'About Paint',
+        action: MenuActionType.ABOUT_PAINT,
+      },
+    ],
+  },
+];
+
+export const NON_MENU_SHORTCUTS: MenuItem[] = [
+  {
+    name: 'Deselect',
+    hotkeys: ['Escape'],
+    action: MenuActionType.DESELECT,
+  },
+];
--- ts-paint/src/app/services/ts-paint/ts-paint.store.state.ts
+++ ts-paint/src/app/services/ts-paint/ts-paint.store.state.ts
@@ -1,4 +1,3 @@
-import { MenuActionType } from '../../types/menu/menu-action-type';
 import { MenuItem } from '../../types/menu/menu-item';
 import { createImage } from '../../helpers/image.helpers';
 import { DrawingTool } from '../../types/drawing-tools/drawing-tool';
@@ -7,6 +6,7 @@ import { DrawingToolType } from '../../types/drawing-tools/drawing-tool-type';
 import { TsPaintAction } from '../../types/actions/ts-paint-action';
 import { Point } from '../../types/base/point';
 import { MoveSelectionTool } from '../../types/drawing-tools/move-selection-tool';
+import { DEFAULT_AVAILABLE_COLORS, DRAWING_TOOLS, MENU_STRUCTURE, NON_MENU_SHORTCUTS } from './ts-paint.config';
 
 export class TsPaintStoreState {
   zoom: number = 1;
@@ -19,271 +19,17 @@ export class TsPaintStoreState {
   fileName: string = 'untitled';
   primaryColor: Color = { r: 0, g: 0, b: 0 };
   secondaryColor: Color = { r: 255, g: 255, b: 255 };
-  availableColors: Color[] = [
-    { r: 0, g: 0, b: 0 },
-    { r: 255, g: 255, b: 255 },
-    { r: 128, g: 128, b: 128 },
-    { r: 196, g: 196, b: 196 },
-    { r: 128, g: 0, b: 0 },
-    { r: 255, g: 0, b: 0 },
-    { r: 128, g: 128, b: 0 },
-    { r: 255, g: 255, b: 0 },
-    { r: 0, g: 128, b: 0 },
-    { r: 0, g: 255, b: 0 },
-    { r: 0, g: 128, b: 128 },
-    { r: 0, g: 255, b: 255 },
-    { r: 0, g: 0, b: 128 },
-    { r: 0, g: 0, b: 255 },
-    { r: 128, g: 0, b: 128 },
-    { r: 255, g: 0, b: 255 },
-  ];
+  availableColors: Color[] = DEFAULT_AVAILABLE_COLORS;
   previewAction: TsPaintAction;
   actions: TsPaintAction[] = [];
   selectedDrawingTool: DrawingTool;
-  availableDrawingTools: DrawingToolType[] = [
-    DrawingToolType.rectangleSelect,
-    DrawingToolType.colorFiller,
-    DrawingToolType.colorPicker,
-    DrawingToolType.magnifier,
-    DrawingToolType.pencil,
-    DrawingToolType.line,
-    DrawingToolType.rectangle,
-    DrawingToolType.ellipse,
-  ];
+  availableDrawingTools: DrawingToolType[] = DRAWING_TOOLS;
   undoPointer: number = -1;
   attributesWindowOpen: boolean = false;
   flipRotateWindowOpen: boolean = false;
   aboutPaintWindowOpen: boolean = false;
   mousePosition: Point;
   unsavedChanges: boolean = false;
-  menuStructure: MenuItem[] = [
-    {
-      name: 'File',
-      menus: [
-        {
-          name: 'New',
-          hotkeys: ['Ctrl', 'N'],
-          action: MenuActionType.NEW,
-        },
-        {
-          name: 'Open...',
-          hotkeys: ['Ctrl', 'O'],
-          action: MenuActionType.OPEN_FILE,
-        },
-        {
-          name: 'Save',
-          hotkeys: ['Ctrl', 'S'],
-          action: MenuActionType.SAVE_FILE,
-        },
-        {
-          name: 'Save As...',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'Print Preview',
-          disabled: true,
-        },
-        {
-          name: 'Page Setup...',
-          disabled: true,
-        },
-        {
-          name: 'Print...',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'Set As Wallpaper (Tiled)',
-          disabled: true,
-        },
-        {
-          name: 'Set As Wallpaper (Centered)',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'Recent file',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'Exit',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      name: 'Edit',
-      menus: [
-        {
-          name: 'Undo',
-          hotkeys: ['Ctrl', 'Z'],
-          action: MenuActionType.UNDO,
-        },
-        {
-          name: 'Repeat',
-          hotkeys: ['Ctrl', 'Y'],
-          action: MenuActionType.REPEAT,
-        },
-        {},
-        {
-          name: 'Cut',
-          hotkeys: ['Ctrl', 'X'],
-          action: MenuActionType.CUT,
-        },
-        {
-          name: 'Copy',
-          hotkeys: ['Ctrl', 'C'],
-          action: MenuActionType.COPY,
-        },
-        {
-          name: 'Paste',
-          hotkeys: ['Ctrl', 'V'],
-          disabled: true,
-        },
-        {
-          name: 'Clear Selection',
-          hotkeys: ['Delete'],
-          action: MenuActionType.CLEAR_SELECTION,
-        },
-        {
-          name: 'Select All',
-          hotkeys: ['Ctrl', 'A'],
-          action: MenuActionType.SELECT_ALL,
-        },
-        {},
-        {
-          name: 'Copy To',
-          disabled: true,
-        },
-        {
-          name: 'Paste From...',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      name: 'View',
-      menus: [
-        {
-          name: 'Tool Box',
-          disabled: true,
-        },
-        {
-          name: 'Color Box',
-          disabled: true,
-        },
-        {
-          name: 'Status Bar',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'Zoom',
-          disabled: true,
-          menus: [
-            {
-              name: 'Normal Size',
-              disabled: true,
-            },
-            {
-              name: 'Large Size',
-              disabled: true,
-            },
-            {
-              name: 'Custom...',
-              disabled: true,
-            },
-            {},
-            {
-              name: 'Show Grid',
-              disabled: true,
-            },
-            {
-              name: 'Show Thumbnail',
-              disabled: true,
-            },
-          ],
-        },
-        {
-          name: 'View Bitmap',
-          disabled: true,
-        },
-        {
-          name: 'Text Toolbar',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      name: 'Image',
-      menus: [
-        {
-          name: 'Flip/Rotate',
-          hotkeys: ['Ctrl', 'R'],
-          action: MenuActionType.FLIP_IMAGE,
-        },
-        {
-          name: 'Stretch/Skew',
-          disabled: true,
-        },
-        {
-          name: 'Invert Colors',
-          hotkeys: ['Ctrl', 'I'],
-          action: MenuActionType.INVERT_COLORS,
-        },
-        {
-          name: 'Attributes',
-          action: MenuActionType.OPEN_ATTRIBUTES_WINDOW,
-        },
-        {
-          name: 'Clear Image',
-          action: MenuActionType.CLEAR_IMAGE,
-        },
-      ],
-    },
-    {
-      name: 'Options',
-      menus: [
-        {
-          name: 'Edit Colors',
-          disabled: true,
-        },
-        {
-          name: 'Get Colors',
-          disabled: true,
-        },
-        {
-          name: 'Save Colors',
-          disabled: true,
-        },
-        {
-          name: 'Draw Opaque',
-          disabled: true,
-        },
-      ],
-    },
-    {
-      name: 'Help',
-      menus: [
-        {
-          name: 'Help Topics',
-          disabled: true,
-        },
-        {},
-        {
-          name: 'About Paint',
-          action: MenuActionType.ABOUT_PAINT,
-        },
-      ],
-    },
-  ];
-  hiddenHotkeyShortcuts: MenuItem[] = [
-    {
-      name: 'Deselect',
-      hotkeys: ['Escape'],
-      action: MenuActionType.DESELECT,
-    },
-  ];
+  menuStructure: MenuItem[] = MENU_STRUCTURE;
+  hiddenHotkeyShortcuts: MenuItem[] = NON_MENU_SHORTCUTS;
 }
