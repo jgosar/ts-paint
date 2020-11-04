0422f98c15b0953b5fd479ec750f845a66b65972
jgosar <36840705+jgosar@users.noreply.github.com>
2020-11-04 18:11:51 +0100

    Cleaned up code

--- ts-paint-chrome-extension/background.js
+++ ts-paint-chrome-extension/background.js
@@ -1,10 +1,8 @@
-const CONTEXT_MENU_ID = "MY_CONTEXT_MENU";
+const TS_PAINT_MENU_ID = "TS_PAINT_MENU";
 function openImage(info,tab) {
-  if (info.menuItemId !== CONTEXT_MENU_ID) {
+  if (info.menuItemId !== TS_PAINT_MENU_ID) {
     return;
   }
-  console.log("Image was clicked.");
-  console.log(info);
   chrome.tabs.create({  
     url: "https://jgosar.github.io/ts-paint/?imageUrl=" + encodeURIComponent(info.srcUrl)
   });
@@ -12,6 +10,6 @@ function openImage(info,tab) {
 chrome.contextMenus.create({
   title: "Edit in TS Paint", 
   contexts:["image"], 
-  id: CONTEXT_MENU_ID
+  id: TS_PAINT_MENU_ID
 });
 chrome.contextMenus.onClicked.addListener(openImage);
